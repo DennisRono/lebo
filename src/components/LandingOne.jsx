d4ac886d75577f34b403ee58efba2bbcd837f477
DennisRono <dennisrkibet@gmail.com>
2022-07-12 22:17:35 +0300

    layout

--- src/components/LandingOne.jsx
+++ src/components/LandingOne.jsx
@@ -1,9 +1,23 @@
 import React, { Fragment } from 'react'
+import '../styles/css/landingOne.css'
+import CentImage from '../images/jumb_kibet.png'
 
 const LandingOne = () => {
   return (
     <Fragment>
-        <h1>landing One</h1>
+        <div className="landingOne">
+            <div className="landingOneWrapper">
+                <div className="landingOneBackParent">
+                    <div className="landingOneBackLeft"></div>
+                    <div className="landingOneBackRight"></div>
+                    <div className="landingOneCenterImage">
+                        <img src={CentImage} alt="" />
+                    </div>
+                    <header className="headerOne"></header>
+                    <div className="textOverlay"></div>
+                </div>
+            </div>
+        </div>
     </Fragment>
   )
 }
