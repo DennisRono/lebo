6b25e1a429f2e46dd6100b9388fc41ed9afd5792
DennisRono <dennisrkibet@gmail.com>
2022-07-14 13:58:46 +0300

    highlight section one added

---
+++ src/components/Highlight.jsx
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react'
+import '../styles/css/highlight.css'
+
+const Highlight = () => {
+  return (
+    <Fragment>
+        <div className="higlightOne">
+            
+        </div>
+    </Fragment>
+  )
+}
+
+export default Highlight
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,10 +1,12 @@
 import React, { Fragment } from 'react'
+import Highlight from '../components/Highlight'
 import LandingOne from '../components/LandingOne'
 
 const Home = () => {
   return (
     <Fragment>
         <LandingOne />
+        <Highlight />
     </Fragment>
   )
 }
